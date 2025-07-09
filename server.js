e4a9cae1bbd0f8e1732e0df0e4f9752f6974bee0
XploreFuture <xplorefutureinfo@gmail.com>
2025-07-09 18:30:07 +0530

    Update server.js
    const PORT = process.env.PORT || 5000;
    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
-    app.listen(5000, () => console.log("Server running on port 5000"));
+    const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
